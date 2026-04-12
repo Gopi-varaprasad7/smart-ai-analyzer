17e535fe15e2eb7453f87ffcbf4a8fd0dfee962e
Koduri Gopi Varaprasad <kodurigopi98722@gmail.com>
2026-04-12 06:21:54 +0530

    Step 5: Train model and interpret feature importance from coefficients

--- src/model.py
+++ src/model.py
@@ -8,4 +8,5 @@ def train_model(df):
     X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2,random_state =42)
     model = LinearRegression()
     model.fit(X_train,y_train)
+    print("Model Coefficients:", model.coef_)
     return model,X_test,y_test
